7a0021ff5570e8f66f402954a89a1b1b5411aabf
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2023-01-07 21:44:23 +0000

    Dev: Add appBar and menu Drawer to the app
    Notes:
        - All widgets shall be inclosed by the added Scaffold.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_shopping_list/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Placeholder(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Simple Shopping List'),
+        ),
+        drawer: const Drawer(),
+        body: const HomeScreen(),
+      ),
     );
   }
 }
