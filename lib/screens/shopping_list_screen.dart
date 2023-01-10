0c67634ea66d87874b2a2df26409130b5ef5f4af
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2023-01-10 15:38:28 +0000

    Dev: Implement static Shopping List Screen

--- lib/screens/shopping_list_screen.dart
+++ lib/screens/shopping_list_screen.dart
@@ -11,9 +11,53 @@ class ShoppingListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<String> shoppingItems = [];
+    for (var i = 1; i <= 20; i++) {
+      shoppingItems.add("Shopping Item $i");
+    }
+
     return CustomMaterialApp(
       title: name,
-      body: const Placeholder(),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            ListView.separated(
+                itemCount: shoppingItems.length,
+                itemBuilder: ((context, int index) {
+                  return Column(
+                    children: [
+                      ListTile(
+                        title: Text(shoppingItems[index]),
+                        trailing: IconButton(
+                          icon: const Icon(
+                            Icons.delete_forever_rounded,
+                          ),
+                          onPressed: () {},
+                        ),
+                        onTap: () {},
+                      ),
+                      SizedBox(
+                        height: (index == shoppingItems.length - 1) ? 100 : 0,
+                      )
+                    ],
+                  );
+                }),
+                separatorBuilder: (context, index) {
+                  return const SizedBox(
+                    height: 10,
+                  );
+                }),
+            Positioned(
+              bottom: 20,
+              right: 20,
+              child: FloatingActionButton(
+                child: const Icon(Icons.add),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
