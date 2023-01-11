5a4304f2d766f598ce5517a14c9ad3e79e9d32ea
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2023-01-11 18:49:57 +0000

    Dev: Synchronize ShoppingLists between db and UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:simple_shopping_list/main.dart';
 import 'package:simple_shopping_list/models/shopping_list.dart';
+import 'package:simple_shopping_list/objectbox.g.dart';
 import 'package:simple_shopping_list/screens/shopping_list_screen.dart';
 import 'package:simple_shopping_list/widgets/custom_material_app.dart';
 import 'package:simple_shopping_list/widgets/new_shopping_list_card.dart';
@@ -13,7 +14,11 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<ShoppingList> shoppingLists = objectbox.shoppingListBox.getAll();
+  final Stream<List<ShoppingList>> _shoppingListStream = objectbox
+      .shoppingListBox
+      .query()
+      .watch(triggerImmediately: true)
+      .map((query) => query.find());
   bool displayNewShoppingCard = false;
 
   @override
@@ -22,44 +27,54 @@ class _HomeScreenState extends State<HomeScreen> {
       body: SafeArea(
         child: Stack(
             children: [
-                  ListView.separated(
-                      itemCount: shoppingLists.length,
-                      itemBuilder: ((context, int index) {
-                        return Column(
-                          children: [
-                            ListTile(
-                              title: Text(shoppingLists[index].name),
-                              trailing: IconButton(
-                                icon: const Icon(
-                                  Icons.delete_forever_rounded,
-                                ),
-                                onPressed: () {
-                                  objectbox.shoppingListBox
-                                      .remove(shoppingLists[index].id);
-                                },
-                              ),
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: ((context) => ShoppingListScreen(
-                                          name: shoppingLists[index].name,
-                                        )),
+                  StreamBuilder<List<ShoppingList>>(
+                      stream: _shoppingListStream,
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData) {
+                          return const CircularProgressIndicator();
+                        }
+                        return ListView.separated(
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: ((context, int index) {
+                              return Column(
+                                children: [
+                                  ListTile(
+                                    title: Text(snapshot.data![index].name),
+                                    trailing: IconButton(
+                                      icon: const Icon(
+                                        Icons.delete_forever_rounded,
+                                      ),
+                                      onPressed: () {
+                                        objectbox.shoppingListBox
+                                            .remove(snapshot.data![index].id);
+                                      },
+                                    ),
+                                    onTap: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: ((context) =>
+                                              ShoppingListScreen(
+                                                name:
+                                                    snapshot.data![index].name,
+                                              )),
+                                        ),
+                                      );
+                                    },
                                   ),
-                                );
-                              },
-                            ),
-                            SizedBox(
-                              height:
-                                  (index == shoppingLists.length - 1) ? 100 : 0,
-                            )
-                          ],
-                        );
-                      }),
-                      separatorBuilder: (context, index) {
-                        return const SizedBox(
-                          height: 10,
-                        );
+                                  SizedBox(
+                                    height: (index == snapshot.data!.length - 1)
+                                        ? 100
+                                        : 0,
+                                  )
+                                ],
+                              );
+                            }),
+                            separatorBuilder: (context, index) {
+                              return const SizedBox(
+                                height: 10,
+                              );
+                            });
                       }),
                   Positioned(
                     bottom: 20,
