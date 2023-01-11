6dbfbd30374cf31f771fa46ea974718482a6d404
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2023-01-11 19:29:59 +0000

    Dev: Display ShoppingItems dynamically
    Effects:
        - ShoppingItems now are synched between the db and ShoppingListScreen.

--- lib/screens/shopping_list_screen.dart
+++ lib/screens/shopping_list_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:simple_shopping_list/main.dart';
+import 'package:simple_shopping_list/models/shopping_item.dart';
 import 'package:simple_shopping_list/models/shopping_list.dart';
+import 'package:simple_shopping_list/objectbox.g.dart';
 import 'package:simple_shopping_list/widgets/custom_material_app.dart';
 
 class ShoppingListScreen extends StatefulWidget {
@@ -17,41 +20,51 @@ class ShoppingListScreen extends StatefulWidget {
 class _ShoppingListScreenState extends State<ShoppingListScreen> {
   @override
   Widget build(BuildContext context) {
-    List<String> shoppingItems = [];
-    for (var i = 1; i <= 20; i++) {
-      shoppingItems.add("Shopping Item $i");
-    }
+    late Stream<List<ShoppingItem>> _shoppingItemStream = objectbox
+        .shoppingItemBox
+        .query(ShoppingItem_.shoppingList.equals(widget.shoppingList.id))
+        .watch(triggerImmediately: true)
+        .map((query) => query.find());
 
     return CustomMaterialApp(
       title: widget.shoppingList.name,
       body: SafeArea(
         child: Stack(
           children: [
-            ListView.separated(
-                itemCount: shoppingItems.length,
-                itemBuilder: ((context, int index) {
-                  return Column(
-                    children: [
-                      ListTile(
-                        title: Text(shoppingItems[index]),
-                        trailing: IconButton(
-                          icon: const Icon(
-                            Icons.delete_forever_rounded,
-                          ),
-                          onPressed: () {},
-                        ),
-                        onTap: () {},
-                      ),
-                      SizedBox(
-                        height: (index == shoppingItems.length - 1) ? 100 : 0,
-                      )
-                    ],
-                  );
-                }),
-                separatorBuilder: (context, index) {
-                  return const SizedBox(
-                    height: 10,
-                  );
+            StreamBuilder<List<ShoppingItem>>(
+                stream: _shoppingItemStream,
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const CircularProgressIndicator();
+                  }
+                  return ListView.separated(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: ((context, int index) {
+                        return Column(
+                          children: [
+                            ListTile(
+                              title: Text(snapshot.data![index].name),
+                              trailing: IconButton(
+                                icon: const Icon(
+                                  Icons.delete_forever_rounded,
+                                ),
+                                onPressed: () {},
+                              ),
+                              onTap: () {},
+                            ),
+                            SizedBox(
+                              height: (index == snapshot.data!.length - 1)
+                                  ? 100
+                                  : 0,
+                            )
+                          ],
+                        );
+                      }),
+                      separatorBuilder: (context, index) {
+                        return const SizedBox(
+                          height: 10,
+                        );
+                      });
                 }),
             Positioned(
               bottom: 20,
