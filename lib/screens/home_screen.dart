90ffea6cc76a22d2450f2e4c17f927652e992c8b
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2023-01-12 19:58:37 +0000

    Ref: Modify NewShoppingList/ItemCard display logic
    Reason:
        - The logic for whether or not to display the NewShoppingList/ItemCard
        and FloatingActionButton in HomeScreen and ShoppingListScreen is quite
        verbose for what it does and is hard to understand.
    
    Effects:
        - Turn all fields in HomeScreen that not used by other classes
        to private fields.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -18,99 +18,89 @@ class _HomeScreenState extends State<HomeScreen> {
       .query()
       .watch(triggerImmediately: true)
       .map((query) => query.find());
-  bool displayNewShoppingCard = false;
+  bool _displayNewShoppingCard = false;
 
   @override
   Widget build(BuildContext context) {
     return CustomMaterialApp(
       body: SafeArea(
         child: Stack(
-            children: [
-                  StreamBuilder<List<ShoppingList>>(
-                      stream: _shoppingListStream,
-                      builder: (context, snapshot) {
-                        if (!snapshot.hasData) {
-                          return const CircularProgressIndicator();
-                        }
-                        return ListView.separated(
-                            itemCount: snapshot.data!.length,
-                            itemBuilder: ((context, int index) {
-                              return Column(
-                                children: [
-                                  ListTile(
-                                    title: Text(snapshot.data![index].name),
-                                    trailing: IconButton(
-                                      icon: const Icon(
-                                        Icons.delete_forever_rounded,
-                                      ),
-                                      onPressed: () {
-                                        objectbox.shoppingListBox
-                                            .remove(snapshot.data![index].id);
-                                      },
-                                    ),
-                                    onTap: () {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: ((context) =>
-                                              ShoppingListScreen(
-                                                shoppingList:
-                                                    snapshot.data![index],
-                                              )),
-                                        ),
-                                      );
-                                    },
-                                  ),
-                                  SizedBox(
-                                    height: (index == snapshot.data!.length - 1)
-                                        ? 100
-                                        : 0,
-                                  )
-                                ],
-                              );
-                            }),
-                            separatorBuilder: (context, index) {
-                              return const SizedBox(
-                                height: 10,
-                              );
-                            });
-                      }),
-                  Positioned(
-                    bottom: 20,
-                    right: 20,
-                    child: (!displayNewShoppingCard)
-                        ? FloatingActionButton(
-                            child: const Icon(Icons.add),
+          children: [
+            StreamBuilder<List<ShoppingList>>(
+              stream: _shoppingListStream,
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const CircularProgressIndicator();
+                }
+                return ListView.separated(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: ((context, int index) {
+                    return Column(
+                      children: [
+                        ListTile(
+                          title: Text(snapshot.data![index].name),
+                          trailing: IconButton(
+                            icon: const Icon(
+                              Icons.delete_forever_rounded,
+                            ),
                             onPressed: () {
-                              setState(() {
-                                displayNewShoppingCard = true;
-                              });
+                              objectbox.shoppingListBox
+                                  .remove(snapshot.data![index].id);
                             },
-                          )
-                        : const SizedBox(),
-                  ),
-                ] +
-                addNewShoppingListCard()),
+                          ),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: ((context) => ShoppingListScreen(
+                                      shoppingList: snapshot.data![index],
+                                    )),
+                              ),
+                            );
+                          },
+                        ),
+                        SizedBox(
+                          height:
+                              (index == snapshot.data!.length - 1) ? 100 : 0,
+                        )
+                      ],
+                    );
+                  }),
+                  separatorBuilder: (context, index) {
+                    return const SizedBox(
+                      height: 10,
+                    );
+                  },
+                );
+              },
+            ),
+            if (!_displayNewShoppingCard)
+              Positioned(
+                bottom: 20,
+                right: 20,
+                child: FloatingActionButton(
+                  child: const Icon(Icons.add),
+                  onPressed: () {
+                    setState(() {
+                      _displayNewShoppingCard = true;
+                    });
+                  },
+                ),
+              ),
+            if (_displayNewShoppingCard)
+              NewShoppingListCard(
+                context: context,
+                onBack: _handleShoppingListCardOnBack,
+              ),
+          ],
+        ),
       ),
     );
   }
 
-  List<Widget> addNewShoppingListCard() {
-    if (displayNewShoppingCard) {
-      return [
-        NewShoppingListCard(
-          context: context,
-          onBack: _handleShoppingListCardOnBack,
-        ),
-      ];
-    } else {
-      return [];
-    }
-  }
-
   void _handleShoppingListCardOnBack() {
     setState(() {
-      displayNewShoppingCard = false;
+      _displayNewShoppingCard = false;
     });
   }
 }
--- lib/screens/shopping_list_screen.dart
+++ lib/screens/shopping_list_screen.dart
@@ -33,74 +33,78 @@ class _ShoppingListScreenState extends State<ShoppingListScreen> {
       body: SafeArea(
         child: Stack(
           children: [
-                StreamBuilder<List<ShoppingItem>>(
-                    stream: _shoppingItemStream,
-                    builder: (context, snapshot) {
-                      if (!snapshot.hasData) {
-                        return const CircularProgressIndicator();
-                      }
-                      return ListView.separated(
-                          itemCount: snapshot.data!.length,
-                          itemBuilder: ((context, int index) {
-                            return Column(
-                              children: [
-                                ListTile(
-                                  title: Text(
-                                    snapshot.data![index].name,
-                                    style: (snapshot.data![index].checked)
-                                        ? const TextStyle(
-                                            decoration:
-                                                TextDecoration.lineThrough,
-                                            decorationThickness: 2,
-                                            fontWeight: FontWeight.w300,
-                                          )
-                                        : null,
-                                  ),
-                                  trailing: IconButton(
-                                    icon: const Icon(
-                                      Icons.delete_forever_rounded,
-                                    ),
-                                    onPressed: () {
-                                      objectbox.shoppingItemBox
-                                          .remove(snapshot.data![index].id);
-                                    },
-                                  ),
-                                  onTap: () {
-                                    ShoppingItem item = snapshot.data![index];
-                                    item.checked = !item.checked;
-                                    objectbox.shoppingItemBox.put(item);
-                                  },
-                                ),
-                                SizedBox(
-                                  height: (index == snapshot.data!.length - 1)
-                                      ? 100
-                                      : 0,
-                                )
-                              ],
-                            );
-                          }),
-                          separatorBuilder: (context, index) {
-                            return const SizedBox(
-                              height: 10,
-                            );
-                          });
-                    }),
-                Positioned(
-                  bottom: 20,
-                  right: 20,
-                  child: (!_displayNewItemCard)
-                      ? FloatingActionButton(
-                          child: const Icon(Icons.add),
-                          onPressed: () {
-                            setState(() {
-                              _displayNewItemCard = true;
-                            });
+            StreamBuilder<List<ShoppingItem>>(
+              stream: _shoppingItemStream,
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const CircularProgressIndicator();
+                }
+                return ListView.separated(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: ((context, int index) {
+                    return Column(
+                      children: [
+                        ListTile(
+                          title: Text(
+                            snapshot.data![index].name,
+                            style: (snapshot.data![index].checked)
+                                ? const TextStyle(
+                                    decoration: TextDecoration.lineThrough,
+                                    decorationThickness: 2,
+                                    fontWeight: FontWeight.w300,
+                                  )
+                                : null,
+                          ),
+                          trailing: IconButton(
+                            icon: const Icon(
+                              Icons.delete_forever_rounded,
+                            ),
+                            onPressed: () {
+                              objectbox.shoppingItemBox
+                                  .remove(snapshot.data![index].id);
+                            },
+                          ),
+                          onTap: () {
+                            ShoppingItem item = snapshot.data![index];
+                            item.checked = !item.checked;
+                            objectbox.shoppingItemBox.put(item);
                           },
+                        ),
+                        SizedBox(
+                          height:
+                              (index == snapshot.data!.length - 1) ? 100 : 0,
                         )
-                      : const SizedBox(),
+                      ],
+                    );
+                  }),
+                  separatorBuilder: (context, index) {
+                    return const SizedBox(
+                      height: 10,
+                    );
+                  },
+                );
+              },
+            ),
+            if (_displayNewItemCard)
+              Positioned(
+                bottom: 20,
+                right: 20,
+                child: FloatingActionButton(
+                  child: const Icon(Icons.add),
+                  onPressed: () {
+                    setState(() {
+                      _displayNewItemCard = true;
+                    });
+                  },
                 ),
-              ] +
-              addNewShoppingListCard(),
+              ),
+            if (_displayNewItemCard)
+              NewShoppingItemCard(
+                context: context,
+                onBack: _handleShoppingListCardOnBack,
+                shoppingList: widget.shoppingList,
+              ),
+          ],
         ),
       ),
     );
